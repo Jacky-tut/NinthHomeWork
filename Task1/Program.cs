d11cd328c7dfec7e73e427323e247842a27b5c83
Jacky_tut <netfinnov@mail.ru>
2022-12-29 22:29:46 +0300

    1task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,3 +3,19 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
+
+
+void stringNumber(int number)
+{
+    if (number == 1)
+    {
+        Console.Write(number);
+    }
+    else
+    {
+        Console.Write($"{number}, ");
+        stringNumber(--number);
+    }
+}
+
+stringNumber(8);
